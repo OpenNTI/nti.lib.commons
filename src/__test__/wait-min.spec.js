fc153c42b228b7a34b6f2757f91f9b29bf4c020d
Andrew Ligon <andrew.ligon@nextthought.com>
2016-05-20 14:20:46 -0500

    Add a minWait function to allow a minimum amount of time to pass before continuing a promise

---
+++ src/__test__/wait-min.spec.js
@@ -0,0 +1,12 @@
+import waitMin from '../wait-min';
+
+describe('wait-min tests', () => {
+	it('Fulfills before min wait time, waits at least the min wait time and then resolves with result', (done) => {
+
+	});
+
+
+	it('Fulfills after min wait time, resolves with result', () => {
+
+	});
+});
--- src/wait.js
+++ src/wait.js
@@ -1,3 +1,6 @@
 export default function wait (milliseconds) {
 	return new Promise(resume => setTimeout(()=>resume(), milliseconds));
 }
+
+export const SHORT = 300;
+export const LONG = 5000;
