62529793cbedd706b7f6d57fbfec669c17115262
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-06 12:05:22 -0500

    add autobind utility

---
+++ src/__test__/autobind.spec.js
@@ -0,0 +1,20 @@
+import autobind from '../autobind';
+
+describe('autobind', () => {
+
+	it('should replace the keys with bound functions', () => {
+		const func = function () { return this; };
+		const o = { func };
+
+		autobind(o, 'func');
+
+		const boundFunc = o.func;
+
+		expect(o.func).not.toBe(func);
+		expect(typeof o.func).toEqual('function');
+		expect(o.func()).toBe(o);
+		expect(boundFunc()).toBe(o);
+		expect(func()).not.toBe(o);
+	});
+
+});
---
+++ src/autobind.js
@@ -0,0 +1,16 @@
+/**
+ * Reassigns the named keys of the scope with functions bound-to the scope.
+ *
+ * @param  {*} scope      any value you want to bind to.
+ * @param  {string} methods Names of menthods on the {scope} to bind.
+ * @return {void}
+ */
+export default function autobind (scope, ...methods) {
+	if (!scope) {
+		throw new Error('Must supply a scope!');
+	}
+
+	for (let fn of methods) {
+		scope[fn] = scope[fn].bind(scope);
+	}
+}
