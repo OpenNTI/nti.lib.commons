76dde7fcd0af3d2cec6c5c6013491dc73ef34c54
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-06 12:03:29 -0500

    make fail with "implement me" message

--- src/__test__/wait-min.spec.js
+++ src/__test__/wait-min.spec.js
@@ -2,11 +2,11 @@ import waitMin from '../wait-min';
 
 describe('wait-min tests', () => {
 	it('Fulfills before min wait time, waits at least the min wait time and then resolves with result', (done) => {
-
+		done.fail('implement me');
 	});
 
 
-	it('Fulfills after min wait time, resolves with result', () => {
-
+	it('Fulfills after min wait time, resolves with result', (done) => {
+		done.fail('implement me');
 	});
 });
