42e4b94b58247230bd50f77f2989a0f29d4f88af
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-07 14:30:07 -0500

    Add Time class. Stub out tests for Josh to implement.

---
+++ src/Time.js
@@ -0,0 +1,99 @@
+export const PERIOD = Object.freeze({
+	AM: 'AM',
+	PM: 'PM'
+});
+
+export default class Time {
+
+	constructor (date = new Date()) {
+		Object.defineProperty('date', {
+			configurable: false,
+			enumerable: false,
+			writable: false,
+			value: Object.freeze(new Date(date))
+		});
+
+		return Object.freeze(this);
+	}
+
+	applyTime (date) {
+		const hours = this.getHours();
+		const minutes = this.getMinutes();
+		date = new Date(date);
+		date.setHours(hours, minutes, 0, 0);
+		return date;
+	}
+
+	getHours () {
+		return this.date.getHours();
+	}
+
+	setHours (hours: Number) {
+		const d = new Date(this.date);
+		d.setHours(hours);
+		return new Time(d);
+	}
+
+	getMinutes () {
+		return this.date.getMinutes();
+	}
+
+	setMinutes (minutes: Number) {
+		const d = new Date(this.date);
+		d.setMinutes(minutes);
+		return new Time(d);
+	}
+
+	getPeriod () {
+		return (this.getHours() < 12) ? PERIOD.AM : PERIOD.PM;
+	}
+
+	setPeriod (period: PERIOD) {
+		if (!Object.values(PERIOD).includes(period)) {
+			throw new Error('Must supply a period.');
+		}
+
+		const old = this.getPeriod();
+		let hours = this.getHours();
+
+		if (old === PERIOD.AM && period === PERIOD.PM) {
+			hours += 12;
+		}
+		else if (old === PERIOD.PM && period === PERIOD.AM) {
+			hours -= 12;
+		}
+		else {
+			return this;
+		}
+
+		const d = new Date(this.date);
+		d.setHours(hours);
+		return new Time(d);
+	}
+
+	/////////////////
+
+	incrementHours () {
+		const d = new Date(this.date);
+		d.setHours(d.getHours() + 1);
+		return new Time(d);
+	}
+
+	decrementHours () {
+		const d = new Date(this.date);
+		d.setHours(d.getHours() - 1);
+		return new Time(d);
+	}
+
+	incrementMinutes () {
+		const d = new Date(this.date);
+		d.setMinutes(d.getMinutes() + 1);
+		return new Time(d);
+	}
+
+	decrementMinutes () {
+		const d = new Date(this.date);
+		d.setMinutes(d.getMinutes() - 1);
+		return new Time(d);
+	}
+}
---
+++ src/__test__/Time.spec.js
@@ -0,0 +1,22 @@
+import Time from '../Time';
+
+describe('Time', () => {
+
+	it('Time intializes with current time by default', () => {});
+	it('Time intializes with given date`s time', () => {});
+
+	it('Setters do not mutate. and return new Time instance (if time changes)', () => {});
+
+	it('GetHours always in 24-hours', () => {});
+	it('GetMinutes', () => {});
+
+	it('SetPeriod correctly converts AM => PM', () => {});
+	it('SetPeriod correctly converts PM => AM', () => {});
+
+	it('incrementHours rolls', () => {});
+	it('incrementMinutes rolls', () => {});
+
+	it('decrementHours rolls', () => {});
+	it('decrementMinutes rolls', () => {});
+
+});
