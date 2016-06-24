a65197c0da07d98a31177d7e3e736de83610bb65
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-24 10:23:52 -0500

    add a helper function to decide if a event is from a click or a keyboard "do this" action

---
+++ src/is-event-actionable.js
@@ -0,0 +1,11 @@
+export default function isEventActionable (e) {
+	return !e ||
+			e.type === 'click' ||
+			(
+				e.type === 'keydown' &&
+				(
+					e.key === 'Enter' ||
+					e.key === ' '
+				)
+			);
+}
