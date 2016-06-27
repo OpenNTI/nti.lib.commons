985687f8401b7fa4ccded724573b060f3a59576f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-27 11:24:41 -0500

    meh

--- src/identify-objects.js
+++ src/identify-objects.js
@@ -2,9 +2,9 @@ export const getID = o => typeof o === 'object' ? o.getID() : o;
 
 export default function matcherFactory (object) {
 	return function (o) {
-		const idOrName = getID(o);
+		const id = getID(o);
 		const otherId = getID(object);
 
-		return idOrName === otherId;
+		return id === otherId;
 	};
 }
