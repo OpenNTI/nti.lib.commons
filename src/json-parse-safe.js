2806949f531eb2f07800955417a4ec242f2acd70
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-05 12:30:48 -0500

    Add safe json parser

---
+++ src/json-parse-safe.js
@@ -0,0 +1,13 @@
+/**
+ * Parses JSON without throwing an exception if there was a problem. Simply returns null instead.
+ *
+ * @param  {string} string The value to be parsed into json
+ * @return {*} The result of parsing.
+ */
+export default function parseJSONSafely (string) {
+	try {
+		return JSON.parse(string || 'null');
+	} catch (e) {
+		return null;
+	}
+}
