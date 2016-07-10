539e73d231a6c4e2a711ce9ddd28caa70bba1696
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-10 11:22:12 -0500

    don't replace <p> with new-lines.
    Mobile Questions/Parts components need to update to ensure the element they render the content into is a block & valid to contain block elements.

--- src/sanitize-markup.js
+++ src/sanitize-markup.js
@@ -1,20 +1,20 @@
 import isEmpty from 'isempty';
 
 const anchors = /<a([^>]*)>(.*?)<\/a>/igm;
-const pars = /<(\/)?p([^>]*)>/igm;
+// const pars = /<(\/)?p([^>]*)>/igm;
 
 function stripAnchors (tag, attrs, body) {
 	return attrs.indexOf('data-id') < 0 && (isEmpty(body) || isEmpty(attrs) || attrs.indexOf('href') === -1) ? '' : tag;
 }
 
-function stripPars (tag, end) {
-	return end ? '\n' : '';
-}
+// function stripPars (tag, end) {
+// 	return end ? '\n' : '';
+// }
 
 export default function (string) {
 
 	string = string && string.replace(anchors, stripAnchors);
-	string = string && string.replace(pars, stripPars);
+	// string = string && string.replace(pars, stripPars);
 
 	return string.trim();
 }
