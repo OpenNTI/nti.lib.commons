dca63c7b82d218a2250650453a5322edb99d58d2
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-12 12:18:08 -0500

    Add get-key-code function

---
+++ src/get-key-code.js
@@ -0,0 +1,27 @@
+/**
+ * Given an object (React Event-like) produce a key-command sequence identifier.
+ *
+ * @param  {objcet} e The event/event-like object to derive the key command from.
+ * @param  {boolean} e.altKey - a flag that states if the alt key is active.
+ * @param  {boolean} e.ctrlKey - a flag that states if the control key is active.
+ * @param  {string} e.key - A string representation of they key. eg: 'S'
+ * @param  {boolean} e.metaKey - a flag that states if the meta/super/command key is active.
+ * @param  {boolean} e.shiftKey - a flag that states if the shift key is active.
+ * @return {string|null} Returns the key-command or null if the object did not represent keys.
+ */
+export default function getKeyCode (e) {
+	const modifiers = ['ctrlKey', 'altKey', 'shiftKey', 'metaKey'];
+	const code = [];
+
+	for (let mod of modifiers) {
+		if (e[mod]) {
+			code.push(mod.replace(/Key$/, ''));
+		}
+	}
+
+	if (e.key) {
+		code.push(e.key.toLowerCase());
+	}
+
+	return code.length ? code.join('-') : null;
+}
