e1a67f03420d8043775c2493cafa5b843a340640
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-08-10 12:33:29 -0500

    add a custom PropType: deprecated Will warn when the prop it is assigned to is used.

---
+++ src/deprecated-prop-type.js
@@ -0,0 +1,9 @@
+export default function deprecated (props, propName, componentName) {
+	if (props[propName] != null) {
+		return new Error(`Used a deprecated prop "${propName}". See ${componentName} component for for details.`);
+	}
+}
+
+deprecated.isRequired = function () {
+	throw new Error('Deprecated fields cannot be required.');
+};
