f439a71b9a0c537ad6a67f846f26b803e291c9eb
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-10-03 16:45:47 -0500

    add getRefHandler to jsx utils

--- src/jsx.js
+++ src/jsx.js
@@ -1,3 +1,6 @@
+import Logger from 'nti-util-logger';
+const logger = Logger.get('common:jsx-utils');
+
 /**
  * Utilitity to abstract away setting raw content on a React component. The content MUST be valid and sanitized.
  *
@@ -10,4 +13,24 @@ export const rawContent = x => ({dangerouslySetInnerHTML: {__html: x}});
 export function sanitizeContent (html) {
 	let d = document.createElement('div');
 	return (d.innerHTML = html, d.innerHTML);
+}
+
+
+export function getRefHandler (parentRef, localRef) {
+	const fn = getRefHandler;
+	const REF_HANDLERS = fn.map || (fn.map = new WeakMap());
+
+	if (typeof parentRef !== 'function') {
+		if (parentRef) {
+			logger.error('getRefHandler has stollen your ref! sorry :/');
+		}
+		return localRef;
+	}
+
+	let h = REF_HANDLERS.get(parentRef);
+	if (!h) {
+		h = (x) => {parentRef(x); localRef(x);};
+		REF_HANDLERS.set(parentRef, h);
+	}
+	return h;
 }
