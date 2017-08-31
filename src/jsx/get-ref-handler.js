5bb931f0dcdc17e6e54e50d10ea1a389e3fcfefb
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-08-31 16:35:22 -0500

    lint

--- src/jsx/get-ref-handler.js
+++ src/jsx/get-ref-handler.js
@@ -16,7 +16,7 @@ export default function getRefHandler (parentRef, localRef) {
 	const PARENT_REF_MAP = REF_HANDLERS.get(localRef) || new WeakMap();
 	REF_HANDLERS.set(localRef, PARENT_REF_MAP);
 
-	const h = PARENT_REF_MAP.get(parentRef) || (x) => {parentRef(x); localRef(x);};
+	const h = PARENT_REF_MAP.get(parentRef) || (x => {parentRef(x); localRef(x);});
 	PARENT_REF_MAP.set(parentRef, h);
 
 	return h;
