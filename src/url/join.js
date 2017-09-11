c4fe4d189b42784c74ebf28e123b0661c3d66ec4
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-09-11 18:16:45 -0500

    fix, add test

--- src/url/join.js
+++ src/url/join.js
@@ -6,15 +6,13 @@ import QueryString from 'query-string';
 export default function join (...parts) {
 	let base = url.parse(parts.shift());
 
-	parts.unshift(base.pathname);
-
 	const urls = parts
 		.filter(Boolean)
 		.map(x => url.parse(x.toString()));
 
 	for(let part of urls) {
 		base.pathname = path.join(base.pathname, part.pathname);
-		base.hash = part.hash;
+		base.hash = part.hash || base.hash;
 		base.search = QueryString.stringify({
 			...QueryString.parse(base.search),
 			...QueryString.parse(part.search),
