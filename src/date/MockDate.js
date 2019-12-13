6612e385c4245db65f7a40e9bc658b27be63e95e
Andrew Ligon <andrew.ligon@nextthought.com>
2019-12-13 11:45:06 -0600

    Add a setNow to the mock date util

--- src/date/MockDate.js
+++ src/date/MockDate.js
@@ -1,19 +1,42 @@
 const GlobalDate = global.Date;
 
 export default class MockDate extends Date {
+	static OriginalDate = GlobalDate;
+
+	static get time () {
+		return this.mockNow ? this.mockNow.getTime() : GlobalDate.now();
+	}
 
 	static install (now = GlobalDate.now()) {
-		this.time = (now instanceof GlobalDate ? now.getTime() : now);
+		this.mockNow = new GlobalDate(now);
 		global.Date = MockDate;
 	}
 
 	static uninstall () {
-		this.time = 0;
+		this.mockNow = null;
 		global.Date = GlobalDate;
 	}
 
 	static now () {
 		return this.time;
+	}
+
+	static setNow (now) {
+		this.mockNow = new GlobalDate(now);
+	}
+
+	static setDestination (dest) {
+		return {
+			hit88MPH: () => {
+				console.log('🚗🔥🔥🔥🔥🔥🔥🔥');//eslint-disable-line
+				this.mockNow = new GlobalDate(dest || 'November 5, 1955');
+			},
+
+			illBeBack: () => {
+				console.log('⚡🔮⚡');//eslint-disable-line
+				this.mockNow = new GlobalDate(dest || 'October 26,1984');
+			}
+		};
 	}
 
 	constructor (...args) {
