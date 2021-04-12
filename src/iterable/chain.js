e1d73525350f22f42aaef05611338dbc862be577
Andrew Ligon <aligon90@gmail.com>
2021-04-12 15:37:21 -0500

    add a utility to chain async iterables

--- src/iterable/chain.js
+++ src/iterable/chain.js
@@ -1,23 +1,32 @@
-/**
- * This is primarily used to combine several iterators together. Lazy iterators welcome.
- *
- * @param {*} input any input. If iterable, will iterate it. If the element of the iterable is iterable it will recurse and iterate that.
- * @returns {Iterator}
- */
-export function* chain(input) {
-	if (!input || !input[Symbol.iterator]) {
-		yield input;
-		return;
-	}
-
-	const it = input[Symbol.iterator]();
-	let current;
-	do {
-		current = it.next();
-		if (current.done) {
+async function* asyncChain (...generators) {
+	for (let generator of generators) {
+		if (!generator || !(generator[Symbol.asyncIterator] ?? generator[Symbol.iterator])) {
+			yield await generator;
 			continue;
 		}
 
-		yield* chain(current.value);
-	} while (!current.done);
+		for await (let value of generator) {
+			yield value;
+		}
+	}
+}
+
+chain.async = asyncChain;
+/**
+ * This is primarily used to combine several iterators together. Lazy iterators welcome.
+ *
+ * @param {...*} generators any input. If iterable, will iterate it. If the element of the iterable is iterable it will recurse and iterate that.
+ * @returns {Iterator}
+ */
+export function* chain (...generators) {
+	for (let generator of generators) {
+		if (!generator || !generator[Symbol.iterator]) {
+			yield generator;
+			continue;
+		}
+
+		for (let value of generator) {
+			yield value;
+		}
+	}
 }
