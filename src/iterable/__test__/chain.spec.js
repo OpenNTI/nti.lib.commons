6dde45fe092577baecfc8fc48d27b802855d0fbd
Jonathan Grimes <jsg2021@gmail.com>
2021-04-13 09:04:36 -0500

    add a very exposed example test case to show how an asyncIterator is different from regular (sync) iterators

--- src/iterable/__test__/chain.spec.js
+++ src/iterable/__test__/chain.spec.js
@@ -2,7 +2,7 @@
 import { chain } from '../chain.js';
 import { wait } from '../../wait/wait';
 
-async function* makeAsyncIterable (values) {
+async function* makeAsyncIterable(values) {
 	for (let value of values) {
 		await wait(1);
 
@@ -10,12 +10,12 @@ async function* makeAsyncIterable (values) {
 	}
 }
 
-async function makeAsyncValue (value) {
+async function makeAsyncValue(value) {
 	await wait(1);
 	return value;
 }
 
-async function resolveAsyncIterable (iter) {
+async function resolveAsyncIterable(iter) {
 	const resolved = [];
 
 	for await (let value of iter) {
@@ -25,7 +25,39 @@ async function resolveAsyncIterable (iter) {
 	return resolved;
 }
 
+const asyncIterable = {
+	[Symbol.asyncIterator]() {
+		return {
+			i: 0,
+			next() {
+				if (this.i < 3) {
+					return Promise.resolve({ value: this.i++, done: false });
+				}
+
+				return Promise.resolve({ done: true });
+			},
+		};
+	},
+};
+
+const syncIterable = {
+	[Symbol.iterator]() {
+		return {
+			i: 0,
+			next() {
+				if (this.i < 3) {
+					return { value: this.i++, done: false };
+				}
+
+				return { done: true };
+			},
+		};
+	},
+};
+
 test('Iterable chain', () => {
+	expect([...chain(syncIterable)]).toEqual([0, 1, 2]);
+	expect([...chain(syncIterable, syncIterable)]).toEqual([0, 1, 2, 0, 1, 2]);
 	expect([...chain([1], 2, [3, 4], [5, 6])]).toEqual([1, 2, 3, 4, 5, 6]);
 	expect([...chain([1])]).toEqual([1]);
 	expect([...chain(1)]).toEqual([1]);
@@ -39,7 +71,12 @@ test('Async Iterable chain', async () => {
 		return [...r];
 	};
 
+	expect(await t(asyncIterable)).toEqual([0, 1, 2]);
+	expect(await t(asyncIterable, asyncIterable)).toEqual([0, 1, 2, 0, 1, 2]);
+
 	expect(await t(makeAsyncValue(1))).toEqual([1]);
 	expect(await t(makeAsyncIterable([1, 2, 3]))).toEqual([1, 2, 3]);
-	expect(await t(makeAsyncIterable([1, 2, 3]), 4, makeAsyncValue(5))).toEqual([1, 2, 3, 4, 5]);
+	expect(
+		await t(makeAsyncIterable([1, 2, 3]), 4, makeAsyncValue(5))
+	).toEqual([1, 2, 3, 4, 5]);
 });
