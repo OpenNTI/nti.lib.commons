7b789a2e8c0ae06e8916584a25db86d819b54bc4
Jonathan Grimes <jsg2021@gmail.com>
2021-04-16 17:35:23 -0500

    make chain add some metadata, if orignal fn is missing just return the toBeChaind.

--- src/function/chain.js
+++ src/function/chain.js
@@ -1,7 +1,12 @@
 export function chain(original, toChain) {
-	return function () {
-		let r = original.apply(this, arguments);
-		toChain.apply(this, arguments);
-		return r;
-	};
+	return !original
+		? toChain
+		: Object.assign(
+				function () {
+					let r = original.apply(this, arguments);
+					toChain.apply(this, arguments);
+					return r;
+				},
+				{ chained: true }
+		  );
 }
